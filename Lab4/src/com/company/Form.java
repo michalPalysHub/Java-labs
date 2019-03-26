abe9f332b477db2d911752c48845ce69a8128e83
michalPalysHub <47611042+michalPalysHub@users.noreply.github.com>
2019-03-26 15:45:27 +0100

    updated version

--- Lab4/src/com/company/Form.java
+++ Lab4/src/com/company/Form.java
@@ -6,15 +6,18 @@ import java.awt.*;
 
 public class Form extends JFrame{
 
-    public Form(){
-        super("SciCalculator");
+    private Form(){
+        setTitle("SciCalculator");
         double height = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
         double width = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         setBounds( (int)width/4,(int)height/4, (int)width/2, (int)height/2);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        initComponents();
         setResizable(true);
         setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
+    private void initComponents(){
 
     }
 
