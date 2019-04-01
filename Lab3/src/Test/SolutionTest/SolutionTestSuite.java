b881d21193d6bf4584ea5f65342d59ea44875840
michalPalysHub <47611042+michalPalysHub@users.noreply.github.com>
2019-04-01 18:22:03 +0200

    updated version

---
+++ Lab3/src/Test/SolutionTest/SolutionTestSuite.java
@@ -0,0 +1,16 @@
+package Test.SolutionTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        SolutionTest1.class,
+        SolutionTest2.class,
+        SolutionTest3.class,
+        SolutionTest4.class,
+})
+
+public class SolutionTestSuite {
+}
