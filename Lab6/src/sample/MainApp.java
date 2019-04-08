f7286921aff57d833992d1b1103017f80ddfd8c4
michalPalysHub <47611042+michalPalysHub@users.noreply.github.com>
2019-04-08 16:58:48 +0200

    KandbanModel added

--- Lab6/src/sample/MainApp.java
+++ Lab6/src/sample/MainApp.java
@@ -2,17 +2,54 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainApp extends Application {
 
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("myKanban");
 
+        initRootLayout();
+        showMainView();
     }
 
+    // Root layout initialization
+
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/root_layout.fxml"));
+            this.rootLayout = (BorderPane) loader.load();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showMainView(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/main_view.fxml"));
+            AnchorPane mainView = (AnchorPane) loader.load();
+
+            this.rootLayout.setCenter(mainView);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ Lab6/src/sample/model/Priority.java
@@ -0,0 +1,7 @@
+package sample.model;
+
+public enum Priority {
+    LOW,
+    MEDIUM,
+    HIGH
+}
