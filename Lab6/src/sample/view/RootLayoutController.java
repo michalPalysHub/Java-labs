07049010a569d4cf2c0ce62661e707e295315d64
michalPalysHub <47611042+michalPalysHub@users.noreply.github.com>
2019-04-08 21:45:51 +0200

    commit

--- Lab6/src/sample/view/RootLayoutController.java
+++ Lab6/src/sample/view/RootLayoutController.java
@@ -1,6 +1,7 @@
 package sample.view;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import sample.MainApp;
 
 public class RootLayoutController {
@@ -14,7 +15,16 @@ public class RootLayoutController {
 
     @FXML
     private void handleAbout(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.initOwner(mainApp.getPrimaryStage());
+        alert.setTitle("About author");
+        alert.setHeaderText("This application is brought to you by:");
+        alert.setContentText("Michał Pałys\n" +
+                "AGH University of Science and Technology\n" +
+                "Faculty od Metals Engineering and Industrial Computer Science\n" +
+                "Applied Computer Science");
 
+        alert.showAndWait();
     }
 
     public void setMainApp(MainApp mainApp){
